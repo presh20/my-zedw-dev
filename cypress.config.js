300d5bfdac36519c789b6814edc54e880c127a9c
Ibeagwa Precious <precious.ibeagwa@zedcrest.com>
2025-09-02 21:17:11 +0100

    Onboard with mailosaur api key

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,6 @@
   pageLoadTimeout : 100000,
   viewportHeight : 700,
   viewportWidth : 1500,
-  
-  
      e2e: {
     //setupNodeEvents(on, config)
     //{
@@ -15,8 +13,12 @@
      baseUrl :'https://dev.business.zedcrestwealth.com',
      
     //}
-
      },
+   "env" :{
+    "MAILOSAUR_API_KEY" :"Y97b8ff7zsGRo2Rr0WnhQr4wCbipkri6"
+   }
+
+
 
      });
 
